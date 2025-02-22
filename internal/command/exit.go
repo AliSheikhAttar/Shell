504ae13ba373541a6a5d877689baff9e58c79f16
AliSheikhAttar <ali.sheikhattar@yahoo.com>
2025-02-22 15:06:36 +0330

    exit argument 0

--- internal/command/exit.go
+++ internal/command/exit.go
@@ -1,8 +1,9 @@
 package command
 
 import (
-    "os"
-    "strconv"
+	"fmt"
+	"os"
+	"strconv"
 )
 
 // ExitCommand implements the 'exit' built-in command
@@ -10,27 +11,30 @@ type ExitCommand struct{}
 
 // NewExitCommand creates a new exit command
 func NewExitCommand() *ExitCommand {
-    return &ExitCommand{}
+	return &ExitCommand{}
 }
 
 // Name returns the name of the command
 func (c *ExitCommand) Name() string {
-    return "exit"
+	return "exit"
 }
 
 // Execute handles the exit command execution
 func (c *ExitCommand) Execute(args []string) error {
-    switch len(args) {
-    case 0:
-        os.Exit(0)
-    case 1:
-        status, err := strconv.Atoi(args[0])
-        if err != nil {
-            return ErrInvalidArgs
-        }
-        os.Exit(status)
-    default:
-        return ErrTooManyArgs
-    }
-    return nil // This line will never be reached due to os.Exit
+	switch len(args) {
+	case 0:
+		fmt.Println("exit status 0")
+		os.Exit(0)
+
+	case 1:
+		status, err := strconv.Atoi(args[0])
+		if err != nil {
+			return ErrInvalidArgs
+		}
+		fmt.Printf("exit status %d\n", status)
+		os.Exit(0)
+	default:
+		return ErrTooManyArgs
+	}
+	return nil // This line will never be reached due to os.Exit
 }

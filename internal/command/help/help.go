a2097a220a4c55d4d2485bb586595100f0aa0935
saba <saba_kianoosh@comp.iust.ac.ir>
2025-02-26 19:33:23 +0330

    change help

--- internal/command/help/help.go
+++ internal/command/help/help.go
@@ -21,29 +21,36 @@ func (c *HelpCommand) Execute(args []string, stdout io.Writer) error {
 	if len(args) > 0 {
 		return utils.ErrInvalidArgs
 	}
-	commands := map[string]string{
-		"color":   "set on/off color mode",
-		"cat":     "see the content of the files",
-		"ls":      "see the content of the directory",
-		"cd":      "change your directory",
-		"echo":    "write text/variables to output",
-		"type":    "type of a command",
-		"pwd":     "current directory path",
-		"exit":    "exit the shell",
-		"history": "history of executed commands",
-		"adduser": "register user to shell",
-		"login":   "login to shell as user",
-		"logout":  "logout the shell",
+	// Updated map to include usage for each command
+	commands := map[string][2]string{
+		
+		"cd":      {"change your directory", "cd <path>"},
+		"ls":      {"see the content of the directory", "ls [options]"},
+		"cat":     {"see the content of the files", "cat <filename>"},
+		"pwd":     {"current directory path", "pwd"},
+		"type":    {"type of a command", "type <command>"},
+		"adduser": {"register user to shell", "adduser {username} {password | empty}"},
+		"echo":    {"write text/variables to output", "echo <text>"},
+		"login":   {"login to shell as user", "login {username} {password | empty}"},
+		"logout":  {"logout the shell", "logout"},
+		"exit":    {"exit the shell", "exit [status code]"},
+		"color":   {"set on/off color mode", "color [on|off]"},
+		"history": {"history of executed commands", "history | history clean"},
 	}
-	fmt.Fprintln(stdout, "---------------------------------------------------")
+
+	fmt.Fprintln(stdout, "-------------------------------------------------------------------------------------------")
+	// Adding the new "Usage" header
 	coloredCommand := utils.ColorText("Command", utils.TextBlue, utils.Bold)
 	coloredDescription := utils.ColorText("Description", utils.TextBlue, utils.Bold)
-	fmt.Fprintf(stdout, "|   %s   |             %s           |\n", coloredCommand, coloredDescription)
-	fmt.Fprintln(stdout, "---------------------------------------------------")
+	coloredUsage := utils.ColorText("Usage", utils.TextBlue, utils.Bold)
+	// Format now includes the "Usage" column
+	fmt.Fprintf(stdout, "|   %s   |             %s           |           %s                       |\n", coloredCommand, coloredDescription, coloredUsage)
+	fmt.Fprintln(stdout, "-------------------------------------------------------------------------------------------")
 	for key, val := range commands {
 		coloredKey := utils.ColorText(key, utils.TextYellow)
-		fmt.Fprintf(stdout, "| %-20s | %-42s |\n", coloredKey, utils.ColorText(val, utils.TextMagenta))
+		// The third element (val[1]) is the "Usage" information
+		fmt.Fprintf(stdout, "| %-20s | %-42s | %-46s |\n", coloredKey, utils.ColorText(val[0], utils.TextMagenta), utils.ColorText(val[1], utils.TextCyan))
 	}
-	fmt.Fprintln(stdout, "---------------------------------------------------")
+	fmt.Fprintln(stdout, "-------------------------------------------------------------------------------------------")
 	return nil
 }
